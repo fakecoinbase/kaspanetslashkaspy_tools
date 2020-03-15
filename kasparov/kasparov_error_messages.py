9bdbe05b2db6b3c752aaaef26ae49021f6649ecd
sava-dag <52160197+sava-dag@users.noreply.github.com>
2020-03-15 18:47:35 +0200

    Kasparov checkup meeting resolutions (#7)
    * [QA-50][QA-49] update module kasparov_error_messages.py:
    
    - removed period at the end of all error messages
    - updated error for "blocks" & "transactions" requests when requests values are empty
    
    * [QA-48] update module kasparov_error_messages.py:
    
    - add variable: blocks_skip_minus_one_error_code
    - add variable: blocks_skip_minus_one_error_message
    - add variable: transactions_skip_minus_one_error_code
    - add variable: transactions_skip_minus_one_error_message
    
    * [QA-43] update module kasparov_error_messages.py:
    
    - update error message per latest updates from product and dev.
    
    * [QA-43] update module kasparov_error_messages.py:
    
    - update error messages to meet latest requirements

--- kasparov/kasparov_error_messages.py
+++ kasparov/kasparov_error_messages.py
@@ -5,34 +5,41 @@ server_error_message = "a server error occurred"
 
 # /Blocks Error
 blocks_limits_error_code = 400
-blocks_limits_error_message = "limit higher than 100 or lower than 1 was requested."
+blocks_limits_error_message = "limit higher than 100 or lower than 0 was requested"
 
 blocks_empty_parameters_error_code = 422
-blocks_empty_order_error_message = "Couldn't parse the 'order' query parameter"
-blocks_empty_skip_error_message = "Couldn't parse the 'skip' query parameter"
-blocks_empty_limit_error_message = "Couldn't parse the 'limit' query parameter"
+blocks_empty_order_error_message = "'' is not a valid order"
+blocks_empty_skip_error_message = "'' is not a valid skip"
+blocks_empty_limit_error_message = "'' is not a valid limit"
+
+blocks_skip_minus_one_error_code = 400
+blocks_skip_minus_one_error_message = "skip lower than 0 was requested"
 
 # /block/{hash} Errors
 block_was_not_found_error_code = 404
 block_was_not_found_error_message = "no block with the given block hash was found"
 
 block_hash_not_hex_encoded_error_code = 422
-block_hash_not_hex_encoded_error_message = "the given block hash is not a hex-encoded 32-byte hash."
+block_hash_not_hex_encoded_error_message = "the given block hash is not a hex-encoded 32-byte hash"
 
 # /utxos/{address} Error
 utxos_address_not_well_formatted_error_code = 422
-utxos_address_not_well_formatted_error_message = "The given address is not a well-formatted P2PKH or P2SH address."
+utxos_address_not_well_formatted_error_message = "The given address is not a well-formatted P2PKH or P2SH address"
 
 # /transactions/address/{address} Errors
 transactions_limits_error_code = 400
-transactions_limits_error_message = "Limit higher than 1000 or lower than 1 was requested."
+transactions_limits_error_message = "Limit higher than 1000 or lower than 0 was requested"
 
 transactions_address_not_well_formatted_error_code = 422
-transactions_address_not_well_formatted_error_message = "The given address is not a well-formatted P2PKH or P2SH address."
+transactions_address_not_well_formatted_error_message = "The given address is not a well-formatted P2PKH or P2SH address"
 
 transactions_empty_parameters_error_code = 422
-transactions_empty_skip_error_message = "Couldn't parse the 'skip' query parameter"
-transactions_empty_limit_error_message = "Couldn't parse the 'limit' query parameter"
+transactions_empty_skip_error_message = "'' is not a valid skip"
+transactions_empty_limit_error_message = "'' is not a valid limit"
+
+transactions_skip_minus_one_error_code = 422
+transactions_skip_minus_one_error_message = "skip lower than 0 was requested"
+
 
 # /fee-estimates Error
 # Only uses the server error code 500
@@ -42,14 +49,14 @@ transaction_id_not_found_error_code = 404
 transaction_id_not_found_error_message = "no transaction with the given txid was found"
 
 transaction_id_not_hex_encoded_error_code = 422
-transaction_id_not_hex_encoded_error_message = "The given txid is not a hex-encoded 32-byte hash."
+transaction_id_not_hex_encoded_error_message = "The given txid is not a hex-encoded 32-byte hash"
 
 # /transaction/hash/{txhash} Errors
 transaction_hash_not_found_error_code = 404
 transaction_hash_not_found_error_message = "no transaction with the given txhash was found"
 
 transaction_hash_not_hex_encoded_error_code = 422
-transaction_hash_not_hex_encoded_error_message = "The given txhash is not a hex-encoded 32-byte hash."
+transaction_hash_not_hex_encoded_error_message = "The given txhash is not a hex-encoded 32-byte hash"
 
 # POST /transaction Errors
 transaction_not_well_formatted_error_code = 422
