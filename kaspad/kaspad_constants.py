0764d465f153d8f7ee519ed7a3bdacfdbc27b565
Elichai Turkel <elichai.turkel@gmail.com>
2020-06-08 14:06:38 +0300

    Modify kaspad_constants to use KASPAD_TOP_PATH (#25)

--- kaspad/kaspad_constants.py
+++ kaspad/kaspad_constants.py
@@ -2,22 +2,18 @@
 This file holds all the constant elements that are being used by the kaspa-d automation.
 """
 import os
-from kaspy_tools import kaspy_tools_constants
+from kaspy_tools.kaspy_tools_constants import KASPAD_TOP_PATH
 
 # Path for the local KASPAD folder
-KASPAD_LOCAL_PATH = kaspy_tools_constants.LOCAL_GO_PATH + '/src/github.com/kaspanet/kaspad'
-
-# Path for the local run-dev folder
-RUNDEV_LOCAL_PATH = os.path.expanduser("~/GoProjects/src/github.com/kaspanet/stuff/run-dev")
 
 # Path for the binary file of a valid empty block
-VALID_BLOCK_EMPTY_PATH = os.path.expanduser("~/GoProjects/src/github.com/kaspanet/kaspy_tools/kaspad/blocks_cartridge/valid_block_empty.dat")
+VALID_BLOCK_EMPTY_PATH = KASPAD_TOP_PATH + "/blocks_cartridge/valid_block_empty.dat"
 
 # Path for the binary file of valid 1848 block
-VALID_1848_BLOCKS_PATH = os.path.expanduser("~/GoProjects/src/github.com/kaspanet/kaspy_tools/kaspad/blocks_cartridge/1848_blocks.dat")
+VALID_1848_BLOCKS_PATH = KASPAD_TOP_PATH + "/blocks_cartridge/1848_blocks.dat"
 
 # Path for the binary file of valid 2138 block
-VALID_2138_BLOCKS_PATH = os.path.expanduser("~/GoProjects/src/github.com/kaspanet/kaspy_tools/kaspad/blocks_cartridge/2138_blocks.dat")
+VALID_2138_BLOCKS_PATH = KASPAD_TOP_PATH + "/blocks_cartridge/2138_blocks.dat"
 
 # The max value possible of uint64
 MAX_UINT64 = 0xffffffffffffffff
@@ -33,4 +29,4 @@ FINALITY_DEVNET = 1000
 PHANTOM_K = 10
 
 # Block has string (hex) slice, to be used in logs, drawing etc.
-PARTIAL_HASH_SIZE= -10
+PARTIAL_HASH_SIZE = -10
