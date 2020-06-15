7bc2f8e5ed70c011662cef82e731de346ff9e4d2
Elichai Turkel <elichai.turkel@gmail.com>
2020-06-15 11:25:27 +0300

    Add a check_in_mempool function (#50)

--- kaspad/kaspad_tx_utils.py
+++ kaspad/kaspad_tx_utils.py
@@ -3,10 +3,12 @@ This module holds the methods that handle all the kaspa-d tx utils for the autom
 """
 from kaspy_tools.kaspad import json_rpc_client
 from kaspy_tools.logs import config_logger
-from kaspy_tools.utils import general_utils
-
+from kaspy_tools.kaspad.json_rpc import json_rpc_requests
 
 KT_logger = config_logger.get_kaspy_tools_logger()
+
+
+RPC_INTERNAL_ERROR = -32603
 
 
 def error_handler(response_error):
@@ -35,3 +37,33 @@ def submit_valid_raw_tx(tx_hex, options=None, conn=None):
     """
     response, response_json = json_rpc_client.submit_raw_transaction_request(tx_hex, options, conn=conn)
     return response, response_json
+
+
+def check_if_in_mempool(tx_id, conn):
+    """
+    Checks if the txID is in the mempool.
+    Returns the tx json if it's in, and the error if not.
+    Will raise if the tx is conflicting or invalid.
+
+    :param tx_id: the txID as hex.
+    :param conn: the RPC connection.
+    :return: response_json or None
+    """
+    in_mempool = json_rpc_requests.get_mempool_entry_request(tx_id, conn)
+    if in_mempool['result']:
+        if in_mempool['error']:
+            raise ValueError(f'Got both result and error in the response: {in_mempool}')
+        return in_mempool['result']
+    else:
+        error_code = in_mempool['error']['code']
+        if error_code != RPC_INTERNAL_ERROR:
+            raise ValueError(
+                f'Missing tx in mempool, expected error code: {RPC_INTERNAL_ERROR}, found: {error_code}')
+
+        expected_error_message = 'transaction is not in the pool'
+        error_message = in_mempool['error']['message']
+        if error_message != expected_error_message:
+            raise ValueError(
+                f'Missing tx in mempool, expected error message: "{expected_error_message}", found: "{error_message}"')
+
+        return None
