d5968d312dffe10dad2a9d354dc916b7d1ac987c
Yuval Shaul <yuval.shaul@gmail.com>
2020-06-25 15:30:40 +0300

    fixing bugs on build big dag. (#67)

--- kaspad/kaspa_dags/big_dag_builder.py
+++ kaspad/kaspa_dags/big_dag_builder.py
@@ -7,7 +7,7 @@ def generate_very_big_dag(*, block_count):
     run_services.docker_compose_rm('kaspad-builder-1', 'kaspad-builder-2')
     run_services.run_docker_compose('kaspad-builder-1', 'kaspad-builder-2')
     docker_compose_data = docker_compose_utils.read_docker_compose_file()
-    conns = docker_compose_utils.get_cons_from_docker_compose(docker_compose_data)
+    conn = docker_compose_utils.get_cons_from_docker_compose(docker_compose_data)['kaspad-builder-1']
     current_blocks_count = json_rpc_requests.get_block_dag_info_request(conn=conn)['result']['blocks']
     while current_blocks_count < block_count:
         dag_make.make_dag(floors=500, min_width=1, max_width=1, conn=conn)
