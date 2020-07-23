a53893370f5ecc411dea44d9042de3af30521d2d
Yuval Shaul <yuval.shaul@gmail.com>
2020-07-23 16:40:28 +0300

    refining scripts (#119)

--- install-1.sh
+++ install-1.sh
@@ -4,23 +4,37 @@
 # The script ends phase 1 of the installation and reboots.
 # After reboot run install-2.sh
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 # Install python 3.8 with all prerequisites
+printf "%s\n" "${grn}Installing python (3.8) globally.${end}"
+
 sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.8 -y
 sudo apt-get install python3.8-dev -y
 
+
 # Install pip3
+printf "%s\n" "${grn}Installing and upgrading pip3.${end}"
 sudo apt install python3-pip -y
 
 # Upgrade pip3
 python3.8 -m pip install --upgrade pip
 
 # install virtualenv
+printf "%s\n" "${grn}Installing virtualenv.${end}"
 sudo python3.8 -m pip install virtualenv
 
 #install docker
+printf "%s\n" "${grn}Installing docker.${end}"
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -40,7 +54,9 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # Configure permissions for docker
+printf "%s\n" "${grn}Configure permissions for docker.${end}"
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
 newgrp docker
+printf "%s\n" "${grn}===>>> REBOOT <<<===${end}"
 systemctl reboot
--- install-2.sh
+++ install-2.sh
@@ -6,22 +6,35 @@
 
 # fill git usename and password (to get  automation_testing repository)
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+printf "%s\n" "${grn}Install docker-compose.${end}"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+printf "%s\n" "${grn}Create kaspanet directory.${end}"
 mkdir kaspanet
 cd kaspanet || exit
 
 #create your virtual environment and activate it
+printf "%s\n" "${grn}Create and activate virtualenv under kaspanet.${end}"
 virtualenv -p "$(which python3.8)" venv
 source venv\bin\activate
 
 # Clone repositories
+printf "%s\n" "${grn}Clone public repositories (without automation_testing).${end}"
 git clone https://github.com/kaspanet/kaspy_tools
 git clone https://github.com/kaspanet/kaspad
 git clone https://github.com/kaspanet/kasparov
 
 
 # install kaspy_tools dependencies
+printf "%s\n" "${grn}Install kaspy_tools requirements.${end}"
 cd kaspy_tools || exit
 pip install -r requirements.txt
 
@@ -29,6 +42,7 @@ pip install -r requirements.txt
 # First make sure graphviz compilation will pass
 sudo apt-get install libgraphviz-dev -y
 
+printf "%s\n" "${red}$"
 printf "To install automation_testing manually:"
 printf "git clone git@github.com:kaspanet/automation_testing.git"
 printf "or:"
@@ -36,5 +50,6 @@ printf "https://github.com/kaspanet/automation_testing.git"
 printf "Then:"
 printf "cd automation_testing"
 printf "pip install -r requirements.txt"
+printf "%s\n" "${end}"
 
 
